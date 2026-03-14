cccdf4f02067f61ef8675c2db133ed4ee5633801
azhark <azharazvtp@gmail.com>
2026-03-14 20:16:20 +0530

    Added transaction attachements

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -9,7 +9,9 @@ abstract class TransactionService {
   factory TransactionService(Dio dio, {String baseUrl}) = _TransactionService;
 
   @POST('/api/transactions/search')
-  Future<List<Transaction>> searchTransactions(@Body() Map<String, dynamic> criteria);
+  Future<List<Transaction>> searchTransactions(
+    @Body() Map<String, dynamic> criteria,
+  );
 
   @GET('/api/transactions/{id}')
   Future<Transaction> getTransactionById(@Path() int id);
@@ -18,8 +20,34 @@ abstract class TransactionService {
   Future<Transaction> createTransaction(@Body() Transaction transaction);
 
   @PUT('/api/transactions/{id}')
-  Future<Transaction> updateTransaction(@Path() int id, @Body() Transaction transaction);
+  Future<Transaction> updateTransaction(
+    @Path() int id,
+    @Body() Transaction transaction,
+  );
 
   @DELETE('/api/transactions/{id}')
   Future<void> deleteTransaction(@Path() int id);
+
+  @GET('/api/transactions/{id}/attachments')
+  Future<List<TransactionAttachment>> getAttachments(@Path('id') int id);
+
+  @POST('/api/transactions/{id}/attachments')
+  @MultiPart()
+  Future<TransactionAttachment> uploadAttachment(
+    @Path('id') int id,
+    @Part(name: 'file') MultipartFile file,
+  );
+
+  @DELETE('/api/transactions/{transactionId}/attachments/{attachmentId}')
+  Future<void> deleteAttachment(
+    @Path('transactionId') int transactionId,
+    @Path('attachmentId') int attachmentId,
+  );
+
+  @GET('/api/transactions/{transactionId}/attachments/{attachmentId}/download')
+  @DioResponseType(ResponseType.bytes)
+  Future<List<int>> downloadAttachment(
+    @Path('transactionId') int transactionId,
+    @Path('attachmentId') int attachmentId,
+  );
 }
