1c9d558edc67ead8f204b1747feeb239f19f59ce
azhark <azharazvtp@gmail.com>
2026-03-29 22:32:23 +0530

    fixing build issue

--- lib/core/storage/token_storage.dart
+++ lib/core/storage/token_storage.dart
@@ -2,14 +2,17 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class TokenStorage {
   static const _storage = FlutterSecureStorage(
-    aOptions: AndroidOptions(encryptedSharedPreferences: true),
+    aOptions: AndroidOptions(),
     iOptions: IOSOptions(accessibility: KeychainAccessibility.first_unlock),
   );
-  
+
   static const _accessTokenKey = 'access_token';
   static const _refreshTokenKey = 'refresh_token';
 
-  static Future<void> saveTokens({required String accessToken, required String refreshToken}) async {
+  static Future<void> saveTokens({
+    required String accessToken,
+    required String refreshToken,
+  }) async {
     await _storage.write(key: _accessTokenKey, value: accessToken);
     await _storage.write(key: _refreshTokenKey, value: refreshToken);
   }
